2682d2e695317c6431f2aef20463a2a0ba5864d6
Fabien Udriot <fabien@omic.ch>
2018-07-20 15:14:46 +0200

    Release 0.2.0 - https://github.com/Ecodev/mailing/compare/0.1.0...0.2.0
    Compatibility TYPO3 v8

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,7 +7,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'fabien@ecodev.ch',
     'module' => '',
     'state' => 'beta',
-    'version' => '0.2.0-dev',
+    'version' => '0.2.0',
     'autoload' => [
         'psr-4' => ['Fab\\Mailing\\' => 'Classes']
     ],
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = [
         [
             'depends' =>
                 [
-                    'typo3' => '7.6.0-7.99.99',
+                    'typo3' => '7.6.0-8.7.99',
                     'vidi' => '2-0.0-0.0.0',
                     'messenger' => '0.9.0-0.0.0',
                 ],
