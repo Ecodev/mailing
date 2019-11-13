265f9277c85be6581960a728bc1e7b5789079ef3
Fabien Udriot <fabien@omic.ch>
2019-11-13 15:20:13 +0100

    [BUGFIX] Fix icon path computing

--- Classes/Backend/Wizard.php
+++ Classes/Backend/Wizard.php
@@ -25,7 +25,7 @@ class Wizard
     function proc($wizardItems)
     {
         $wizardItems['plugins_tx_mailing_message'] = array(
-            'icon' => ExtensionManagementUtility::extRelPath('mailing') . 'Resources/Public/Images/Mailing.png',
+            'icon' => 'EXT:mailing/Resources/Public/Images/Mailing.png',
             'title' => $this->getLanguageService()->sL('LLL:EXT:mailing/Resources/Private/Language/locallang.xlf:wizard.title'),
             'description' => $this->getLanguageService()->sL('LLL:EXT:mailing/Resources/Private/Language/locallang.xlf:wizard.description'),
             'params' => '&defVals[tt_content][CType]=list&defVals[tt_content][list_type]=mailing_messages'
