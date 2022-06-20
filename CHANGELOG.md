00f0da35defddbd473172297790a6c227e98dc7b
Fabien Udriot <fabien@omic.ch>
2022-06-20 08:49:51 +0200

    Release 1.0.1 - <https://github.com/Ecodev/mailing/compare/1.0.0...1.0.1>

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+## 1.0.1 (2022-06-20)
+
+[TASK] Fix ext_localconf and ext_tables
+[TASK] Prepare next release
--- ext_emconf.php
+++ ext_emconf.php
@@ -2,9 +2,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Mailing list',
     'description' => 'Authenticated FE users can send bunch of messages to a list of recipients. This list is defined as a dynamic selection in a FE module (Vidi).',
-    'category' => 'plugin',
     'author' => 'Fabien Udriot',
     'author_email' => 'fabien@ecodev.ch',
     'state' => 'stable',
-    'version' => '1.1.0-dev',
 ];
